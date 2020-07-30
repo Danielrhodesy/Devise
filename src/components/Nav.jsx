5a04348c84c9af65f2e7c1ae5c89b04607ecf772
Daniel Rhodes <danielrhodesnz@gmail.com>
2020-07-30 16:41:56 +1200

    Nav bar scss

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,19 +5,32 @@ class Nav extends Component {
   render() {
     return (
       <>
-        <div class="nav-container sb">
-            <div class="logo-container fe">
+        <div class="nav-container flex sa">
+            <div class="container sb">
               <div class="logo">
               </div>
-            </div>
-            <nav>
-              <div class="nav-item">  
+              <nav class="se ct">
+              <div class="nav-item se ct">  
+                <p>About</p>
               </div>
-              <div class="nav-item">  
+              <div class="nav-item se ct">
+                <p>Filter</p>  
               </div>
-              <div class="nav-item">  
+              <div class="nav-item se ct">  
+                <p>Search</p>
+              </div>
+              <div class="nav-item se ct profile"> 
+                <p>P</p> 
+              </div>
+              <div class="nav-item se ct"> 
+                <p>setting</p>
+              </div>
+              <div class="nav-item post-proj se ct">
+                <p>Post Project</p>  
               </div>
             </nav>
+            </div>
+            
         </div>
       </>
     );
