6adf139d6aa25c3559e4193fba01d64b34057b17
Daniel Rhodes <danielrhodesnz@gmail.com>
2020-08-02 11:54:20 +1200

    NHompage Components, Find Project

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -8,6 +8,7 @@ class HomePage extends Component {
     return (
       <>
         <Nav/>
+        <HomePage/>
       </>
     );
   }
---
+++ src/components/Findproject.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+
+class Findproject extends Component {
+  render() {
+    return (
+      <>
+        <section class="fp-container bl sa ct">
+            <article>
+                <h1>Find a Project</h1>
+                <div class="search"></div>
+                <div class="button">Search</div>   
+            </article>
+        </section>
+      </>
+    );
+  }
+}
+
+export default (Findproject)
